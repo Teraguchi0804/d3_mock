c0fa43562429d854e844acd0d959fa33e0c3bcd5
Norimaki <norimaki.tera@gmail.com>
2017-01-05 17:29:03 +0900

    init

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+var d3 = require("d3");
+
+var dataSet = [300, 130, 5, 60, 240];
+
+d3.select("#myGraph")
+    .selectAll("rect")
+    .data(dataSet)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d, i){
+      return i * 25;
+    })
+    .attr("width", function (d,i) {
+      return d + "px";
+    })
+    .attr("height","20px");
